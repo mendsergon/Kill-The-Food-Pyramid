ad623e346fc08cb991e15e20b73042daa119e8f6
mendsergon <skaloumenos70@protonmail.com>
2025-07-11 18:53:01 +0300

    Added bullet collision logic, made bullets move per second and not at fps

--- Assets/Scripts/bullet_1.gd
+++ Assets/Scripts/bullet_1.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+@onready var ray_cast_2d: RayCast2D = $RayCast2D  # Raycast to detect collisions manually
+
 ### --- CORE CONSTANTS --- ###
-const SPEED: int = 450                      # Bullet travel speed (pixels per second)
+const SPEED: int = 300                     # Bullet travel speed (pixels per second)
 
 ### --- MOVEMENT --- ###
-func _process(delta: float) -> void:
-	position += transform.x * SPEED * delta  # Move bullet forward based on local direction
+func _physics_process(delta: float) -> void:
+	if ray_cast_2d.is_colliding():             # Check if raycast hit something this frame
+		var collider = ray_cast_2d.get_collider()
+		print("Hit:", collider.name)
+		queue_free()                            # Destroy bullet on impact
+	else:
+		position += transform.x * SPEED * delta  # Move bullet forward based on local direction
 
 ### --- LIFETIME MANAGEMENT --- ###
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
